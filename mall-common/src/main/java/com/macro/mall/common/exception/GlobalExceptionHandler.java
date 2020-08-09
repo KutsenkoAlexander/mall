8af4894a1cfa82a67e5e0fedf956856d70bf7c20
alexander <kucenkoaleksandr@gmail.com>
2020-08-09 14:15:19 +0300

    Translated

--- mall-common/src/main/java/com/macro/mall/common/exception/GlobalExceptionHandler.java
+++ mall-common/src/main/java/com/macro/mall/common/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 全局异常处理
+ * Глобальная обработка исключений
  * Created by macro on 2020/2/27.
  */
 @ControllerAdvice
